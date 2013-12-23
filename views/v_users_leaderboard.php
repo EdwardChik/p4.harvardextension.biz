7ff8b863b6eb243438753791b1825b330045361e
Edward Chik <edward.chik@gmail.com>
2013-12-22 23:18:10 -0500

    Updated readme.MD

--- views/v_users_leaderboard.php
+++ views/v_users_leaderboard.php
@@ -13,8 +13,6 @@
 		<!-- loops through users database to display each one, and generating an HTML5 table to apply the DataTables plugin onto -->
 		<?php foreach($users as $user): ?>
 
-			<?php if ($user['games_played'] != 0) { $average_hp = $user['hp_remaining'] / $user['games_played']; } ?>
-
 	        <tr>
 	            <!-- user name -->
 	            <td><?=$user['first_name']?> <?=$user['last_name']?></td>
